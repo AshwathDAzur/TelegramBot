bb0a62acda0c52dee58e97ab61d15ddd7837d94d
Ashwath kumaran <a4ashwath@gmail.com>
2024-02-28 11:11:34 +0530

    Scraped successfully from the webpage - minimal processed and sending to the user

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
+const cheerio = require('cheerio');
+const axios = require('axios');
 
+const url = process.env.SCRAP_URL;
 const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
     const input_txt = msg.text.toLowerCase().replace(/\n|\r/g, "").trim();
     console.log("Bot Input Message :" + input_txt);
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Hi, Thank you for chatting with me');
+    try {
+        var replyData = await webscrap();
+        replyData.forEach(reply => {
+            const chatId = msg.chat.id;
+            bot.sendMessage(chatId, reply);
+        });
+    } catch (error) {
+        console.log(error);
+    }
 });
+
+async function webscrap() {
+    try {
+        var response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+        // Select the div with class "cb-col-100 cb-col cb-schdl"
+        const divElement = $(".cb-col-100.cb-col.cb-schdl");
+        // console.log(divElement.html());
+        const rawData = divElement.text();
+        const rawArray = rawData.split("    ");
+        const filteredArray = rawArray.filter(item => item.trim().length > 0);
+        const newArray = [`***Live Scores - by Ashwath***`, ...filteredArray];
+        return newArray;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
--- scrap.js
+++ scrap.js
@@ -7,8 +7,17 @@ async function webscrap() {
     try {
         const response = await axios.get(url);
         const $ = cheerio.load(response.data);
-        const val = $("div").text();
-        console.log(val);
+        // Select the div with class "cb-col-100 cb-col cb-schdl"
+        const divElement = $(".cb-col-100.cb-col.cb-schdl");
+        // console.log(divElement.html());
+        const rawData = divElement.text();
+        const rawArray = rawData.split("    ");
+        const filteredArray = rawArray.filter(item => item.trim().length > 0);
+        console.log(filteredArray);
+        filteredArray.forEach(match => {
+            console.log(match);
+        });
+
     }
     catch (error) {
         console.log(error);
